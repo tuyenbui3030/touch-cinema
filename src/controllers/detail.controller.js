16364cbf5bd20804ed733d4effc82a78de7b37c4
tuyenbui3030 <tuyenbui3030@gmail.com>
2021-05-22 19:37:27 +0700

    done page detail

--- src/controllers/detail.controller.js
+++ src/controllers/detail.controller.js
@@ -1,5 +1,12 @@
+const { Movie, Cinema, CinemaPhoto } = require("../models");
+
 module.exports = {
   index: async (req, res) => {
-    res.render("detail/index");
+    const movie = await Movie.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.render("detail/index", { movie });
   },
 };
--- src/routes/detail.route.js
+++ src/routes/detail.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const detailController = require("../controllers/detail.controller");
 const router = express.Router();
 
-router.get("/", detailController.index);
+router.get("/:id", detailController.index);
 
 module.exports = router;
